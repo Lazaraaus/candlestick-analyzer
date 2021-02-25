cb9701f26cec64082ac6e4b0364a6bb91ea19821
Lazaraaus <wood.yjaden@gmail.com>
2021-02-25 08:00:39 -0500

    Initial Commit, candlestick-analyzer

---
+++ app.py
@@ -0,0 +1,85 @@
+# IMPORTS 
+import os, csv
+import yfinance as yf
+import pandas as pd
+import talib 
+from flask import Flask, render_template, request, flash, redirect, jsonify
+from patterns import candlestick_patterns
+
+# CONSTANTS 
+STOCK_PATH = 'datasets/daily/Stocks'
+CRYPTO_PATH = 'datasets/daily/Crypto'
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def index():
+    # Get Pattern entered by user
+    pattern = request.args.get('candlestick_pattern', None)
+    # Create empty dict to hold stocks data 
+    stocks = {}
+    # Open dataset     
+    with open('datasets/companies.csv') as f:
+        # Store Company Symbol as key in Stocks dict
+        # Value is another dict containing the company name 
+        for row in csv.reader(f):
+            stocks[row[0]] = {'company': row[1]}
+
+    # If User selected a pattern
+    if pattern:
+        # Get list of CSV files for companies
+        datafiles = os.listdir('datasets/daily/Stocks')
+        # Loop through them and read each into a pandas Data Frame
+        for filename in datafiles:
+            dataframe = pd.read_csv(f'datasets/daily/Stocks/{filename}')
+            # Get a reference to the pattern analysis function used by TA-LIB 
+            pattern_function = getattr(talib, pattern)
+           
+            symbol = filename.split('.')[0]
+            try:
+                # Process data frame
+                result = pattern_function(dataframe["Open"], dataframe["High"], dataframe["Low"], dataframe["Close"])
+                # Get the value of the last data frame (most recent)
+                last = result.tail(1).values[0]
+            
+                # If the value is nonzero, the dataframe triggered the pattern
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:
+                    stocks[symbol][pattern] = None
+            except:
+                pass
+            # Print all stock data to console 
+            print(stocks)
+    return render_template('index.html', candle_patterns=candlestick_patterns, stocks=stocks, current_pattern=pattern) 
+
+# Route to grab stock, crypto data  
+@app.route('/snapshot')
+def snapshot():
+    """Take a snapshot of the daily closes of s&p500, binance bitcoin exchange"""
+    # Open list of companies and cryptos we're collect data for 
+    with open('datasets/companies.csv') as f, open('datasets/cryptos.csv') as curr:
+        companies = f.read().splitlines()
+        cryptos = curr.read().splitlines() 
+        # Extraneous data member
+        companies.remove(companies[-1])
+        
+        # Create and loop counter 
+        index = 0
+        # Loop through companies and download price data from yahoo to CSV 
+        for company in companies:
+            symbol = company.split(',')[0]
+            dataframe = yf.download(symbol, start="2021-01-01", end="2021-01-27")
+            dataframe.to_csv('datasets/daily/Stocks/{}.csv'.format(symbol))
+            # If within index of crypto list, download price data for cryptos also 
+            if index < len(cryptos):
+                symbol2 = cryptos[index].split(',')[0]
+                dataframe = yf.download(symbol2, start="2021-01-01", end="2021-01-27")
+                dataframe.to_csv('datasets/daily/Crypto/{}.csv'.format(symbol))
+                index += 1
+    return {
+        'code': 'success'
+    }
--- pattern_detect.py
+++ pattern_detect.py
@@ -1,19 +1,26 @@
+# IMPORTS 
 import talib
 import yfinance as yf
-data = yf.download("GME", start="2020-01-01", end="2021-01-27")
 
+# TESTING CANDLESTICK PATTERN ANALYSIS WITH YFINANCE + TA-LIB 
+
+# Download Data Frame of asset index
+data = yf.download("GME", start="2020-12-01", end="2021-01-27")
+
+# Get Morning star pattern analysis
 morning_stars = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-
+# Get Engulfing pattern analysis
 engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
-
+# Get moment indicator 
 momentum = talib.ADX(data['High'], data['Low'], data['Close'], timeperiod=20)
-
+# Get Evening star pattern analysis 
 evening_stars = talib.CDLEVENINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-
+# Get Relative Strength Index Indicator
 relative_strength = talib.RSI(data['Close'], timeperiod=14)
-
+# Get Exponetial Moving Average Indicator
 exp_moving_avg = talib.EMA(data['Close'], timeperiod=20)
 
+# Assign analysis and indicator vals to Data Frame with titles
 data['Morning_star'] = morning_stars
 data['Engulfing'] = engulfing 
 data['Momentum'] = momentum
@@ -21,17 +28,23 @@ data['EMA'] = exp_moving_avg
 data['RSI'] = relative_strength 
 data['Evening_star'] = evening_stars
 
+# Print data frame
 print(data) 
 
-
+# Break down data frame by respective analysis/indicator
 engulfing_days = data[data["Engulfing"] != 0]
 morning_star_flips = data[data["Morning_star"] != 0]
 evening_star_flips = data[data["Evening_star"] != 0]
 
+# Print data frame for engulfing days analysis
 print("Engulfing days \n")
 print(engulfing_days)
+
+# Print data frame for morning star analysis
 print("Morning star flips \n")
 print(morning_star_flips)
+
+# Print data frame for evening star analysis 
 print("Evening star flips \n")
 print(evening_star_flips)
 
